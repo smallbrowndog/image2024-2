85871473e20615b51c22c60863c10ecd3d9986f6
smallbrowndog <136410944+smallbrowndog@users.noreply.github.com>
2024-10-23 21:26:36 +0900

    Middle

--- 90HaarFaceDetection_copy_task.py
+++ 90HaarFaceDetection_copy_task.py
@@ -18,12 +18,21 @@ faces = face_cascade.detectMultiScale(gray)
 
 # 교수님 최종 정리
 for fx, fy, fw, fh in faces:
-    cv2.rectangle(img, (fx, fy), (fx + fw, fy + fh), (255, 0, 0), 8)
+    # 사각형
+    # cv2.rectangle(img, (fx, fy), (fx + fw, fy + fh), (255, 0, 0), 8)
+
+    # 원
+    # cv2.circle(img, (int(fx + fw / 2), int(fy + fh / 2)), (60), (255, 0, 0), 2)
+
+    # 타원
+    cv2.ellipse(img, (int(fx + fw / 2), int(fy + fh / 2)), (60,80), 0, 0, 360, (255,0,0), 2)
+
+
     eyes = eye_cascade.detectMultiScale(gray[fy:fy+fh, fx:fx+fw])
     # print(eyes)
     for ex, ey, ew, eh in eyes:
         # 사각형
-        # cv2.rectangle(img, (ex + fx, ey + fy), (ex + fx + ew, ey + fy + eh), (0, 255, 0), 2)
+        cv2.rectangle(img, (ex + fx, ey + fy), (ex + fx + ew, ey + fy + eh), (0, 255, 0), 2)
 
         # 원
         # cv2.circle(img, center, radian, color, thickness) 이런 방식으로 동작해야함
@@ -31,7 +40,7 @@ for fx, fy, fw, fh in faces:
         # cv2.circle(img, (int((ex + fx)*2 + ew / 2) - (ex + fx), int((ey + fy)*2 + eh / 2) - (ey + fy)), (20), (0, 255, 0), 2)
 
         # 타원
-        cv2.ellipse(img, (int((ex + fx)*2 + ew / 2) - (ex + fx), int((ey + fy)*2 + eh / 2) - (ey + fy)), (20,10), 0, 0, 360, (0,255,0), 2)
+        # cv2.ellipse(img, (int((ex + fx)*2 + ew / 2) - (ex + fx), int((ey + fy)*2 + eh / 2) - (ey + fy)), (20,10), 0, 0, 360, (0,255,0), 2)
 
 # 폰트 설정 (원하는 경로에 있는 TTF 폰트 파일을 사용)
 fontpath = "./BMJUA_ttf.ttf"  # 원하는 한글 폰트 파일 경로 설정
@@ -44,7 +53,7 @@ img_pil = Image.fromarray(img)
 draw = ImageDraw.Draw(img_pil)
 
 # 한글 텍스트
-draw.text((250, 600), "김기주 중간과제", font=font , fill=(0, 255, 255))
+draw.text((280, 600), "김기주 중간과제", font=font , fill=(0, 255, 255))
 
 # 다시 OpenCV 형식으로 변환
 img = np.array(img_pil)
