5f7e6089b20262c5389a7e724079a1a554bdd37d
108-0 <kkj7388@naver.com>
2024-11-07 15:40:10 +0900

    update

---
+++ 90HaarFaceDetection_copy_task2.py
@@ -0,0 +1,32 @@
+# Retina Face 활용 얼굴인식
+# Haar과 비교
+
+# https://github.com/serengil/retinaface
+
+import cv2
+import matplotlib.pyplot as plt
+from retinaface import RetinaFace
+
+# 원본 이미지 로드
+img_path = './img/man_face.jpg'
+img = cv2.imread(img_path)
+
+retina_img = RetinaFace.detect_faces(img_path)
+
+print(retina_img)
+
+for face in retina_img.keys():
+    face_info = retina_img[face]
+    facial_area = face_info['facial_area']
+    x1, y1, x2, y2 = facial_area
+
+    # 사각형으로 얼굴 표시
+    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 3)
+
+# for face in faces:
+#   plt.imshow(face)
+#   plt.show()
+
+cv2.imshow('gray', img)
+cv2.waitKey()
+cv2.destroyAllWindows()
