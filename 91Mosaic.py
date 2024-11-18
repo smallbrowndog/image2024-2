38f5cc70790558eece9a66ddfeba9b27e33fec1d
108-0 <kkj7388@naver.com>
2024-11-18 15:51:15 +0900

    upload

---
+++ 91Mosaic.py
@@ -0,0 +1,29 @@
+import cv2
+
+rate = 10
+img = cv2.imread('./img/smilings.jpg')
+
+x,y,w,h = cv2.selectROI('mosaic', img, False)
+if w and h:
+    roi = img[y:y+h, x:x+w]
+    roi = cv2.resize(roi, (w//rate, h//rate))
+    roi = cv2.resize(roi, (w, h))
+    img[y:y+h, x:x+w] = roi
+    cv2.imshow('mosaic', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+while True:
+    x, y, w, h = cv2.selectROI('mosaic', img, False)
+    if w and h:
+        roi = img[y:y + h, x:x + w]
+        roi = cv2.resize(roi, (w // rate, h // rate))
+        roi = cv2.resize(roi, (w, h))
+        img[y:y + h, x:x + w] = roi
+        cv2.imshow('mosaic', img)
+    else:
+        break
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
